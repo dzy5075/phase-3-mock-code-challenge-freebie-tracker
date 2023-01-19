22832fbd3510a4f52c095dc8514731484e7e306f
Daniel Yim <daniel.yim96@gmail.com>
2023-01-19 11:16:06 -0500

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,10 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name:"Pencil", value:"1", dev_id: 1, company_id: 1)
+Freebie.create(item_name:"T-shirt", value:"2", dev_id: 2, company_id: 3)
+Freebie.create(item_name:"Hammer", value:"3", dev_id: 3, company_id: 4)
+Freebie.create(item_name:"Flag", value:"4", dev_id: 4, company_id: 2)
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
